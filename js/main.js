f0e498a15a2f267afc93863d5ee4beabc326b70d
Dylan-Jacquet <dylan.jacquet@student.hepl.be>
2022-11-25 15:44:53 +0100

    :tada: Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const buttonsSelector = document.querySelectorAll('button');
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.score = 0;
+    }
+}
+const players = [new Player('Dylan'),new Player('Billy')];
+for (const buttonSelectorElement of buttonsSelector) {
+    buttonSelectorElement.addEventListener('click', (e)=>{
+        players[parseInt(e.currentTarget.dataset.player)].score++;
+        console.table(players);
+    });
+}
+
+/**************************************************************************************/
+// CORRECTION
