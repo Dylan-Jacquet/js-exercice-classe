25a263d71ff3cb1fcfbb3b76d7c9541100b451e5
Dylan-Jacquet <dylan.jacquet@student.hepl.be>
2022-11-25 16:39:18 +0100

    ✅ All done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.score = 0;
-    }
-}
-const buttonsSelector = document.querySelectorAll('button');
-const players = [new Player('Dylan'),new Player('Billy')];
-for (const buttonSelectorElement of buttonsSelector) {
-    buttonSelectorElement.addEventListener('click', (e)=>{
-        players[parseInt(e.currentTarget.dataset.player)].score++;
-        console.table(players);
-    });
-}
+/** CORRECTION by Jacquet Dylan **/
+(function () {
+        class Player {
+            constructor(name) {
+                this.name = name;
+                this.score = 0;
+            }
 
-/**************************************************************************************/
-// CORRECTION
+            incScore() {
+                this.score++;
+            }
+        }
+
+        const game = {
+            buttons: document.querySelectorAll('.play'),
+            players: [new Player('Dylan'), new Player('Billy')],
+            play(e) {
+                const idx = parseInt(e.currentTarget.dataset.player);
+                this.players[idx].incScore();
+                e.currentTarget.textContent = `${this.players[idx].name} : ${this.players[idx].score}`
+            },
+            init() {
+                for (const button of this.buttons) {
+                    button.addEventListener('click', (e) => {
+                        this.play(e);
+                    });
+                }
+            },
+        }
+        game.init()
+    }
+)();
